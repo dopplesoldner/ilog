41f319dcfa097497678d029709652fc9c579adc0
Mohammad Rashid <dopplesoldner@gmail.com>
2015-07-20 13:46:43 +0100

    working linkedin auth

--- www/js/app.js
+++ www/js/app.js
@@ -6,28 +6,61 @@
 var il = angular.module('starter', ['ionic', 'ngCordova']);
 
 il.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 });
 
-il.controller('loginCtrl', function($scope, $cordovaOauth) {
+il.controller('loginCtrl', function($scope, $cordovaOauth, $http) {
 
     $scope.googleLogin = function() {
-        client_id = '581508692768-2578gubf298g8o1ddpr6fpoeli95pag2.apps.googleusercontent.com'
-        $cordovaOauth.google(client_id, ['https://www.googleapis.com/auth/urlshortener', 'https://www.googleapis.com/auth/userinfo.email']).then(function(result) {
+        clientId = '540881881646-2r4cnjid14gn69ant2assi14l2afj9ij.apps.googleusercontent.com'
+        $cordovaOauth.google(clientId, ['https://www.googleapis.com/auth/urlshortener', 'https://www.googleapis.com/auth/userinfo.email']).then(function(result) {
             console.log(JSON.stringify(result));
-            $scope.data = JSON.stringify(result);
+            $scope.googleData = JSON.stringify(result);
         }, function(error) {
             console.log(error);
         });
     }
+
+    $scope.linkedinLogin = function() {
+        clientId = '772h7nkedaqp3a'
+        clientSecret = 'iEf12MqlkmhhZDgo'
+        appScope = ['r_basicprofile', 'r_emailaddress']
+        state = 'login'
+        $cordovaOauth.linkedin(clientId, clientSecret, appScope, state).then(function(result) {
+            console.log(JSON.stringify(result));
+            window.localStorage.setItem("access_token", result.access_token);
+            $scope.linkedinAccessCode= JSON.stringify(result);
+            if(result.access_token) {
+                $scope.linkedinData();
+            }
+        }, function(error) {
+            console.log(error);
+        });
+    }
+
+    $scope.linkedinData = function() {
+        console.log('trying to add data')
+        //apiUrl = 'https://api.linkedin.com/v1/people/~?format=json'
+        apiUrl = 'https://api.linkedin.com/v1/people/~:(id,formatted-name,positions,headline,summary,location,industry,educations,languages,picture-urls::(original))?format=json'
+        $http.defaults.headers.common.Authorization = "Bearer " + window.localStorage.getItem("access_token");
+        $http.get(apiUrl)
+        .success(function(data) {
+            console.log(JSON.stringify(data));
+            $scope.linkedinData= JSON.stringify(data);
+        })
+        .error(function(error) {
+            console.log(error);
+        });
+    }
+
 
 });
